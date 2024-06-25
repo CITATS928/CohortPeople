bb488e58fbd0e8c2a934bed0fd5a05c66ec51c86
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-06-25 19:54:56 -0400

    Ive done this lab

--- src/main/java/zipcode/rocks/Main.java
+++ src/main/java/zipcode/rocks/Main.java
@@ -12,12 +12,13 @@ public class Main {
     private static final String COMMA_DELIMITER = ",";
 
     public static void main(String[] args) {
-        String student_source = "FakeData.csv";
+        String student_source = "TenFiveZero.csv";
 
         // Load data file into an ArrayList<Person> of Person objects.
         Main programObject = new Main();
-
-        List listOfStudents = programObject.loadCSVFile(student_source);
+//ver1
+//        List listOfStudents = programObject.loadCSVFile(student_source);
+        List listOfStudents = programObject.handWrittenLoadOfData();
 
         int numberOfPersons = listOfStudents.size();
         for (int i = 0; i< numberOfPersons; i++){
@@ -41,11 +42,74 @@ public class Main {
         return records;
     }
 
-    private Person getRecordFromLine(String line) {
+    private Person getRecordFromLine ( String line ) {
         // take the String of lastname,firstname and create a new Person object with it.
         String[] values = line.split(COMMA_DELIMITER);
-        Person newPerson = new Person(values[0], values[1]);
-        return newPerson;
+// ver1
+//        Person newPerson = new Person(values[0], values[1], values[2], values[3],values[4],values[5]);
+//        return newPerson;
+
+        //ver2
+        return null;
     }
+
+    // copy from here...
+    private List handWrittenLoadOfData() {
+        ArrayList<Person> students = new ArrayList<>();
+
+        Person std1 = new Person("Teddy", "Archibald");
+        std1.setColor("Red");
+        std1.setBeverage("H&H");
+        std1.setBirthMonth("March");
+        std1.setPet(false);
+        students.add(std1);
+
+        Person std2 = new Person("Qian", "Wang");
+        std2.setColor("purple");
+        std2.setBeverage("Latte");
+        std2.setBirthMonth("Feb");
+        std2.setPet(true);
+        students.add(std2);
+
+        Person std3 = new Person("Teddy", "Archibald");
+        std3.setColor("Red");
+        std3.setBeverage("Coffee");
+        std3.setBirthMonth("March");
+        std3.setPet(false);
+        students.add(std3);
+
+        Person std4 = new Person("Tim", "Linkous");
+        std4.setColor("Green");
+        std4.setBeverage("Water");
+        std4.setBirthMonth("June");
+        std4.setPet(true);
+        students.add(std4);
+
+        Person std5 = new Person("Bryan", "Smith");
+        std5.setColor("Red");
+        std5.setBeverage("Tea");
+        std5.setBirthMonth("White");
+        std5.setPet(false);
+        students.add(std5);
+
+        Person std6 = new Person("Will", "Chapman");
+        std6.setColor("red");
+        std6.setBeverage("water");
+        std6.setBirthMonth("March");
+        std6.setPet(false);
+        students.add(std6);
+
+        Person std7 = new Person("Jared", "Thacker");
+        std7.setColor("green");
+        std7.setBeverage("Devi");
+        std7.setBirthMonth("Dec");
+        std7.setPet(true);
+        students.add(std7);
+
+        // and so on for EACH person in the cohort.
+
+        return students;
+    }
+    //...to here. Paste below the `getRecordFromLine()` method in Main.java
 
 }
--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -3,10 +3,31 @@ package zipcode.rocks;
 public class Person {
     private String firstname;
     private String lastname;
+    private String Color;
+    private String BirthMonth;
+    private Boolean Pet;
+    private String Beverage;
 
+
+
+    //use for ver.1
+    /*
+    public Person(String fname, String lname,String Bev, String FC, String BM, String PT) {
+        this.firstname = fname;
+        this.lastname = lname;
+        this.Beverage=Bev;
+        this.Color=FC;
+        this.BirthMonth=BM;
+        this.Pet=Boolean.valueOf(PT);
+    }
+
+     */
+
+    //use for version two
     public Person(String fname, String lname) {
         this.firstname = fname;
         this.lastname = lname;
+
     }
 
 
@@ -26,7 +47,26 @@ public class Person {
         this.lastname = lastname;
     }
 
+    public String getColor(){return Color;}
+
+    public void setColor(String Color){this.Color=Color;}
+
+    public String getBirthMonth(){return BirthMonth;}
+
+    public void setBirthMonth(String BirthMonth){this.BirthMonth=BirthMonth;}
+
+    public String getBeverage(){return Beverage;}
+
+    public void setBeverage(String Beverage) {this.Beverage=Beverage;}
+
+    public Boolean getPet(){
+        return Pet;
+    }
+
+    public void setPet(Boolean Pet){this.Pet=Pet;}
+
     public String toString() {
-        return this.lastname + ", " + this.firstname;
+        return this.lastname + ", " + this.firstname+". "+"favorite Beverage is "+this.Beverage+
+                ". favorite color is "+this.Color+" . BM is "+this.BirthMonth+". like Pet? "+this.Pet;
     }
 }
